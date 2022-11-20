e419665a58f2c9cdb02e5f07b9ff2f8bb2b03b90
aldih suhandi <aldihsuhandi697@gmail.com>
2022-11-20 20:43:36 +0700

    refactor code

--- src/main/java/com/project/spring/common/util/AssertUtil.java
+++ src/main/java/com/project/spring/common/util/AssertUtil.java
@@ -6,20 +6,24 @@ import com.project.spring.common.model.exception.SpringException;
 public class AssertUtil {
     public static void isExpected(int v1, int v2, String param, SpringErrorCodeEnum errorCodeEnum) throws SpringException {
         if (v1 != v2) {
-            throw new SpringException(String.format("%s value is not expected", param), errorCodeEnum);
+            throwException(String.format("%s value is not expected", param), errorCodeEnum);
         }
     }
 
     public static void isNotExpected(int v1, int v2, String param, SpringErrorCodeEnum errorCodeEnum) throws SpringException {
         if (v1 == v2) {
-            throw new SpringException(String.format("%s value is not expected", param), errorCodeEnum);
+            throwException(String.format("%s value is not expected", param), errorCodeEnum);
         }
     }
 
     public static void isNotNull(Object o, String param, SpringErrorCodeEnum errorCode) throws SpringException {
         if (o == null) {
-            throw new SpringException(String.format("%s value is null", param), errorCode);
+            throwException(String.format("%s value is null", param), errorCode);
         }
+    }
+
+    private static void throwException(String msg, SpringErrorCodeEnum errorCode) throws SpringException {
+        throw new SpringException(msg, errorCode);
     }
 
 }
